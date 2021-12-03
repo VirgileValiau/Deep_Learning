141d6cccc43fcb1871ce52d16cc6a38adf837ace
Virgile <v.valiau@live.fr>
2021-12-03 19:48:15 +0100

    backprop loss

--- TPS/TP2/Loss.py
+++ TPS/TP2/Loss.py
@@ -7,6 +7,7 @@ class Loss:
         self.npr = 0        # length of list preceding
         self.y = y          # array of class labels of the training data
         self.k = k          # layer index
+        self.delta = np.zeros((1,))
         
     def forward(self,i):
         label = onehot(self.y,i)
@@ -16,4 +17,12 @@ class Loss:
         else:
             return -np.log(zin)
         
-    
+    def backprop(self,i):
+        zin = self.preceding[0]
+        if self.y[i] == 0:
+            dli = 1/(1-zin)
+        else:
+            dli =  -1/zin
+        self.delta[0] = dli
+        return dli
+            
